4c6155cfe62d90ae79c7fb85966ec88780da03e8
Dat <dpham@lemonway.com>
2016-12-08 18:00:02 +0100

    2 by default

--- LemonWay/LemonWayAPI.php
+++ LemonWay/LemonWayAPI.php
@@ -823,7 +823,6 @@ class LemonWayAPI
         curl_setopt($ch, CURLOPT_URL, $this->config->dkUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($ch, CURLOPT_TIMEOUT, 60);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $xml_soap);
